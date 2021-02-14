23a0bb5e7b7b2f8a40ef57b1f9f43342558675cc
dsbowen <dsbowen@wharton.upenn.edu>
2021-02-14 11:58:10 -0500

    deploying algorithm

--- survey.py
+++ survey.py
@@ -1,10 +1,23 @@
 from hemlock import Branch, Page, Label, route
+from hemlock.tools import Assigner
+
+assigner = Assigner({
+    'set0': ['A', 'B', 'C', 'D'],
+    'set1': ['E', 'F', 'G', 'H']
+})
 
 @route('/survey')
 def start():
+    conditions = assigner.next()
     return Branch(
         Page(
-            Label('Hello, World!'), 
+            Label('Page '+conditions['set0'])
+        ),
+        Page(
+            Label('Page '+conditions['set1'])
+        ),
+        Page(
+            Label('The end!'), 
             terminal=True
         )
     )
